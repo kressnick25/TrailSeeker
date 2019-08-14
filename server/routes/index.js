c5355a872e8d0de3fbed3dfd2587955a6f312bb2
nkress <kressnick25@gmail.com>
2019-08-14 11:50:50 +1000

    resolved all promises before responding with json

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const createError = require('http-errors');
-import {requestGoogle, requestWeather, requestTrails}  from '../src/request';
-const axios = require('axios')
+import {requestGoogle, requestWeather, requestTrails} from '../src/request';
 
+const axios = require('axios')
 
 
 const DEFAULT_LAT = '-27.436350';
@@ -14,58 +14,51 @@ const DEFAULT_LNG = '153.002120';
 //   res.render('index', { title: 'Trail Seeker' });
 // });
 
-router.post('/', function(req, res, next){
-  // check params have been supplied
-  // if (!req.body.lat || !req.body.lng){
-  //   return next(createError(400, 'lat and lng are required parameters'));
-  // }
-  let lat = req.body.lat;
-  let lng = req.body.lng;
-  const getTrails = requestTrails(lat, lng)
-    .then(response => {
-      if (response.data.results) {
-          // Get weather info for each Trail
-          let trails = response.data.data;
-          let c = 0;
-          for (let i = 0; i < trails.length; i++) {
-            // requestWeather(trails[i].lat, trails[i].lon)
-            //   .then(response =>{
-            //     if (response.data.currently){
-            //       // add currentWeather value to trail object
-            //       trails[i].weather = response.data.daily.summary;
-            //       c++;
-            //     }
-            //     // Wait for all api requests to complete before responding
-            //     // TODO do this with promises 
-            //     if (c  === trails.length - 1){
-            //       res.json(trails);
-            //     }
-            //   })
-            //   .catch(err => console.log("Error in Dark Sky Weather API request\n", err));
-                // Get Google distance info for each Trail
-                requestGoogle(lat, lng, trails[i].lat, trails[i].lon)
-                  .then(response => {
-                    if (response.data.rows){
-                      let elements = response.data.rows[0].elements[0]
-                      trails[i].distance = elements.distance.text;
-                      trails[i].duration = elements.duration.text;
-                      c++;
-                    }
-                    if (c === trails.length - 1) {
-                      res.json(trails);
-                    }
-                  })
-                  .catch(err => console.log("Error in Google Distance API request\n", err))
-              
+router.post('/', function (req, res, next) {
+    // check params have been supplied
+    // if (!req.body.lat || !req.body.lng){
+    //   return next(createError(400, 'lat and lng are required parameters'));
+    // }
+    let lat = req.body.lat;
+    let lng = req.body.lng;
+    requestTrails(lat, lng)
+        .then(response => {
+            if (response.data.results) {
+                // Get weather info for each Trail
+                let trails = response.data.data;
+                let fetches = [];
+                for (let i = 0; i < trails.length; i++) {
+                    fetches.push(
+                        requestWeather(trails[i].lat, trails[i].lon)
+                            .then(response => {
+                                if (response.data.currently) {
+                                    // add currentWeather value to trail object
+                                    trails[i].weather = response.data.daily.summary;
+                                }
+                            })
+                            .catch(err => console.log("Error in Dark Sky Weather API request\n", err))
+                    );
+                    fetches.push(
+                        // Get Google distance info for each Trail
+                        requestGoogle(lat, lng, trails[i].lat, trails[i].lon)
+                            .then(response => {
+                                if (response.data.rows) {
+                                    let elements = response.data.rows[0].elements[0]
+                                    trails[i].distance = elements.distance.text;
+                                    trails[i].duration = elements.duration.text;
+                                }
+                            })
+                            .catch(err => console.log("Error in Google Distance API request\n", err))
+                    );
+                }
+                Promise.all(fetches)
+                    .then(() => res.json(trails));
             }
+        })
+        .catch(err => console.log("Error in Trails API request", err));
 
-         
-        }
-    })
-    .catch(err => console.log("Error in Trails API request", err));
-
-    
 
 });
+
 
 module.exports = router;
