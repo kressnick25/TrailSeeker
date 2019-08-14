d5a081af0a8127544bdb655f7f91bc5b71dec815
nkress <kressnick25@gmail.com>
2019-08-14 20:35:22 +1000

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:10
+
+# Create app dir
+WORKDIR /usr/src/app
+
+#install yarn because
+RUN npm install -g yarn
+
+# move package and lock files
+COPY ./server/package.json ./
+COPY ./server/yarn.lock ./
+
+# install node_modules
+RUN yarn
+
+# copy source code from server folder
+COPY ./server .
+
+EXPOSE 3000
+CMD ["yarn", "start"]
+
+
---
+++ server/build/precache-manifest.98271a8b9b3f6ab281f97ac4867cb5de.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4391e1297e12155efb416e0e2b01803d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8702d7046ca8ab11c29",
+    "url": "/static/css/2.0aaa5e4a.chunk.css"
+  },
+  {
+    "revision": "19a9d679ae49163fbd5f",
+    "url": "/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "d8702d7046ca8ab11c29",
+    "url": "/static/js/2.f8597687.chunk.js"
+  },
+  {
+    "revision": "19a9d679ae49163fbd5f",
+    "url": "/static/js/main.ae4d13a0.chunk.js"
+  },
+  {
+    "revision": "aac10b1cbb2aec953c81",
+    "url": "/static/js/runtime~main.bdc68d36.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "53f709c56c0cb268362ba119c34e072a",
+    "url": "/static/media/clear-day.53f709c5.svg"
+  },
+  {
+    "revision": "b87ca8b626b1d792e4b35da43f795ba7",
+    "url": "/static/media/clear-night.b87ca8b6.svg"
+  },
+  {
+    "revision": "79b3c647bcb4502fe1f95e9d9c5de5d9",
+    "url": "/static/media/cloudy.79b3c647.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "3694ef67610075498c91cdb7b2bfb2f4",
+    "url": "/static/media/fog.3694ef67.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "534afba6dc879efc03f27d5c964416bf",
+    "url": "/static/media/partly-cloudy-day.534afba6.svg"
+  },
+  {
+    "revision": "675a7cddcf441a98bad0880208ecdb37",
+    "url": "/static/media/partly-cloudy-night.675a7cdd.svg"
+  },
+  {
+    "revision": "316ec3310f84545f10f9525b44509d10",
+    "url": "/static/media/rain.316ec331.svg"
+  },
+  {
+    "revision": "35404d3acb64aeb0eb68d9da2e81f3d7",
+    "url": "/static/media/snow.35404d3a.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "2a8661ec0e951cce9158ce43bf453430",
+    "url": "/static/media/wind.2a8661ec.svg"
+  }
+]);
